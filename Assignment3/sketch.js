99f997a3d6d00e8cbac99182a538bc331dbb5281
NLeday1212 <60441852+NLeday1212@users.noreply.github.com>
2022-02-01 14:00:02 -0600

    Assignment 3 V1
    Added outline files and folders

---
+++ Assignment3/sketch.js
@@ -0,0 +1,18 @@
+let img;
+
+function preload(){
+img = loadImage("background.png");
+}
+
+function setup() {
+  createCanvas(900, 450);
+  image(img, 10, 10, 100, 100);
+}
+
+function draw() {
+  
+ 
+
+  
+  
+}
